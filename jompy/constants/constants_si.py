6e93db30a66d6f3cca670c761e645dd182cd59cf
GuillermoNP <guillermo.navas.89@gmail.com>
2014-09-03 23:56:18 +0200

    constants - si

---
+++ jompy/constants/constants_si.py
@@ -0,0 +1,9 @@
+from ..constants.constant import Constant
+
+
+"""
+SI base units: http://en.wikipedia.org/wiki/International_System_of_Units
+
+"""
+
+k_boltzmann = Constant('Boltzmann constant', 1.3806488e-23, 'si', 'J/K', 'm^2*kg/(s^2*K)')
