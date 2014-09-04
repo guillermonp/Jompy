4780431e17639c6cd8452e9ba0cfa363bb9ce1d1
GuillermoNP <guillermo.navas.89@gmail.com>
2014-09-05 01:15:22 +0200

    vacancy functions

--- jompy/matscience/imperfections.py
+++ jompy/matscience/imperfections.py
@@ -1,12 +1,15 @@
 from jompy.constants.constants_si import k_boltzmann, k_boltzmann_ev
+from jompy.functions.core import sigmoid
 import math
 
 
 class PointDefects(object):
+    """
+
+    """
     def __init__(self, unit='J'):
         """
         :param unit: J or eV
-        :return:
         """
         if unit == 'J':
             self.k = k_boltzmann.value
@@ -16,34 +19,43 @@ class PointDefects(object):
             raise TypeError('unit {0} is not accepted. '
                             'Units accepted: J and eV'.format(unit))
 
-    def vacancies(self, n, q, t):
+    def vacancies(self, n, q_v, t):
         """
         :param n: Number of lattice sites
-        :param q: Activation energy - energy for vacancy formation
+        :param q_v: Activation energy - energy for vacancy formation
         :param t: Temperature (K)
         :return: number of vacancies
         """
-        return n * math.exp(-q/(self.k * t))
+        return n * math.exp(-q_v/(self.k * t))
 
-    def frenkel_defect(self, n, q, t):
+    def vacant_probability(self, q_v, t):
         """
-        Neighboring cation vacancy and cation interstitial
-        :return:
+        Probability of finding site in vacant is given by
+            P = sigmoid(-Qv/(kT))
         """
-        return n * math.exp(-q/(2*self.k * t))
+        return sigmoid(-q_v/(self.k * t))
 
-    def activation_energy(self):
+    def frenkel_defect(self, n, q_v, t):
+        """ Neighboring cation vacancy and cation interstitial """
+        return n * math.exp(-q_v/(2*self.k * t))
+
+    def schottky_defect(self):
+        pass
+
+    def exp_q_v(self):
+        """
+        Obtain activation energy experimental
+        """
         pass
 
     @staticmethod
-    def defect_concentration(n, nv):
+    def defect_concentration(n, n_v):
         """
         concentration = Nv/N
             N = number of lattice sites
             Nv = number of vacancies
-        :return:
         """
-        return nv/n
+        return n_v/n
 
     def composition_by_mass(self):
         pass
