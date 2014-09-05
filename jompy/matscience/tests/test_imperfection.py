af11bbb4f11af338253d43584abd27d3333f7d22
GuillermoNP <guillermo.navas.89@gmail.com>
2014-09-06 00:42:41 +0200

    new example

--- jompy/matscience/tests/test_imperfection.py
+++ jompy/matscience/tests/test_imperfection.py
@@ -1,5 +1,22 @@
 from jompy.matscience.imperfections import PointDefects
 
 defects = PointDefects('eV')
-vacancies = defects.vacancies(n=8e28, q=0.9, t=1273)
-defect_ratio = defects.defect_concentration(n=8e28, nv=vacancies)
+
+# Number of vacancies and vacancy concentration
+vacancies = defects.vacancies(n=8e28, q_v=0.9, t=1273)
+edc = defects.equilibrium_concentration(q_v=0.9, t=1293)
+prob = defects.vacant_probability(q_v=0.9, t=1273)
+
+print('vacancies = {0} \n'
+      'equilibrium concentration of defects = {1:.4%} \n'
+      'Probability of finding site in vacant = {2:.4%}'.
+      format(vacancies, edc, prob))
+
+# Number of Frenkel defects per cubic meter in zinc oxide at 1000ºC.
+# Activation energy = 2.51 eV, density for ZnO = 5.55 g/cm3 at that temperature.
+lattice_sites_volume = defects.lattice_sites(density=5.55, molar_mass=81.41)
+# lattice sites per cubic meter
+l_sites_m3 = lattice_sites_volume * 1e6
+frenkel_defects = defects.frenkel_defect(n=l_sites_m3, q_v=2.51, t=1273)
+
+print("frenkel defects = ", frenkel_defects)
