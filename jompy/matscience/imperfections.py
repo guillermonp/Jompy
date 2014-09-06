53b81ab6b22718151765d96212a3fa93efa382f3
GuillermoNP <guillermo.navas.89@gmail.com>
2014-09-06 02:30:23 +0200

    composition

--- jompy/matscience/imperfections.py
+++ jompy/matscience/imperfections.py
@@ -1,6 +1,7 @@
 from jompy.constants.constants_si import k_boltzmann, k_boltzmann_ev, n_avogrado
 from jompy.functions.core import sigmoid, n_comb_k
 import math
+from collections import OrderedDict
 
 
 class PointDefects(object):
@@ -18,8 +19,8 @@ class PointDefects(object):
         The extrinsic defects are solutes or impurities depending if they were
         intentionally added to the material. We can distinguish between
         substitutional solute (impurity) or interstitial solute.
-
     """
+
     def __init__(self, unit='J'):
         """
         :param unit: J or eV
@@ -41,7 +42,7 @@ class PointDefects(object):
         :param t: Temperature (K)
         :return: number of vacancies
         """
-        return n * math.exp(-q_v/(self.k_b * t))
+        return n * math.exp(-q_v / (self.k_b * t))
 
     def equilibrium_concentration(self, q_v, t):
         """
@@ -51,14 +52,14 @@ class PointDefects(object):
 
             Nv/N = exp(-Qv/(kT))
         """
-        return math.exp(-q_v/(self.k_b * t))
+        return math.exp(-q_v / (self.k_b * t))
 
     def vacant_probability(self, q_v, t):
         """
         Probability of finding site in vacant is given by
             P = sigmoid(-Qv/(kT))
         """
-        return sigmoid(-q_v/(self.k_b * t))
+        return sigmoid(-q_v / (self.k_b * t))
 
     def frenkel_defect(self, n, q_v, t):
         """
@@ -66,7 +67,7 @@ class PointDefects(object):
             a neutral defect that is made up of a paired vacancy and interstitial.
             Neighboring cation vacancy and cation interstitial
         """
-        return n * math.exp(-q_v/(2*self.k_b * t))
+        return n * math.exp(-q_v / (2 * self.k_b * t))
 
     def schottky_defect(self, n, q_v, t):
         """
@@ -74,7 +75,7 @@ class PointDefects(object):
             A neutral defect that involves paired vacancies on the cation
             an anion sublattices.
         """
-        return n * math.exp(-q_v/(2*self.k_b * t))
+        return n * math.exp(-q_v / (2 * self.k_b * t))
 
     @staticmethod
     def lattice_sites(density, molar_mass):
@@ -134,11 +135,34 @@ class PointDefects(object):
         """
         return self.k_b * math.log(w)
 
-    def composition_by_mass(self):
-        pass
+    def gibbs_free_energy(self, n, n_v, q_v):
+        """
+        Enthalpy change for the formation of a single defect is
+        the activation energy (Qv) therefore for generate n defects
+        the change is n * Qv. Thus, Gibbs free energy:
+            delta(G) = n * Qv - 2 * k * ln(W)
 
-    def composition_by_mole(self):
-        pass
+            where we are assuming that n_i = n_v
+            (number of interstitial sites = number of vacancies)
+        """
+        return n_v * q_v - 2 * self.w_vacancies(n, n_v)
 
     def __repr__(self):
         return 'Point defects, unit={0}'.format(self.unit)
+
+
+def comp_by_mass(m_a, m_b):
+    """
+
+    :param m_a:
+    :param m_b:
+    """
+    total_weight = m_a + m_b
+    comp_a = m_a / total_weight * 100
+    comp_b = m_b / total_weight * 100
+
+    return [comp_a, comp_b]
+
+
+def comp_by_mole():
+    pass
