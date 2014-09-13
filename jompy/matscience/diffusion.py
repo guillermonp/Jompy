02ac7c14524a487663109d80df6a382c50d9af05
GuillermoNP <guillermo.navas.89@gmail.com>
2014-09-13 23:57:07 +0200

    Fick's fist law - linear approach

--- jompy/matscience/diffusion.py
+++ jompy/matscience/diffusion.py
@@ -15,7 +15,32 @@ class SDiffusion(object):
         """
         pass
 
+    @staticmethod
+    def fick_1law_linear(d, c2, c1, thick):
+        """
+        rate of diffusion independent of time
+
+            Diffusion: [L^2 / time] (e.g. cm^2/s)
+            Concentration: [M / L^3] (e.g. g/cm^3)
+            Thick: (x2 - x1) [L] (e.g. cm)
+
+        Flux in x-direction: cm^2/s * g/cm^3 * 1/cm = g/(cm^2 s)
+
+        :param d: diffusion coefficient
+        :param c2: concentration 2
+        :param c1: concentration 1
+        :param thick:
+        """
+        return - d * (c2 - c1) / thick
+
     def diffusion_coef(self, d0, q_d, t):
+        """
+        Effect of temperature to diffusion
+
+        :param d0: Initial diffusion coefficient
+        :param q_d: Activation energy (J)
+        :param t: Absolute temperature (K)
+        """
         return d0 * math.exp(-q_d / (self._R * t))
 
 
