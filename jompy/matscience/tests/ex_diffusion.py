e34cdf11da3b8a8cd217e7c28f93740ffdb98e42
GuillermoNP <guillermo.navas.89@gmail.com>
2014-09-14 14:13:40 +0200

    example using Fick1

--- jompy/matscience/tests/ex_diffusion.py
+++ jompy/matscience/tests/ex_diffusion.py
@@ -1,6 +1,4 @@
-from jompy.matscience.diffusion import SDiffusion
-
-s_diff = SDiffusion()
+from jompy.matscience.diffusion import Fick1
 
 # Steel plate at 700ºC, in steady-state, how much carbon transfers
 # from the rich to the deficient side?
@@ -8,6 +6,5 @@ C1 = 1.2  # kg/m3 at 5 mm
 C2 = 0.8  # kg/m3 at 10 mm
 D = 3*10**(-11)  # m2/s at 700ºC
 
-flux = s_diff.fick_1law_linear(d=D, c2=C2, c1=C1, thick=0.005)
-print(flux)
-
+flux = Fick1(d=D, c2=C2, c1=C1, thick=0.005).flux()
+print("flux [kg/m2 s] = ", flux)
